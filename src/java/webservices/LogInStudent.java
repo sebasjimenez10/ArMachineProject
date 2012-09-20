cf18487591d7d46585e0cfa07785f5b1036ef696
sebasjimenez10 <sebasjimenezv@gmail.com>
2012-09-20 18:45:22 -0500

    Adding web services

---
+++ src/java/webservices/LogInStudent.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package webservices;
+
+import dao.DAOProfessor;
+import dao.DAOStudent;
+import javax.ejb.Stateless;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
+
+/**
+ *
+ * @author David
+ */
+@Stateless
+@Path("/StudentLogIn")
+public class LogInStudent {
+    
+    @GET
+    public String LogInStudent(@QueryParam("Student") String usr, @QueryParam("Password") String passw) {
+        //invoke el DAO
+        DAOStudent DAOp = new DAOStudent();
+
+        String rst = DAOp.LogInStudent(usr, passw);
+        if (rst == null) {
+            return "Invalid Student";
+        }
+        return rst;
+    }
+    
+}
