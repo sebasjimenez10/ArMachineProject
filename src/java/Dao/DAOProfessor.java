a54b7edfec323e997dea2a42b993e3a3fda38282
David <David@10.0.46.166>
2012-10-10 12:28:46 -0500

    Nueva version, actualizacion

---
+++ src/java/Dao/DAOProfessor.java
@@ -0,0 +1,87 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Dao;
+
+import connection.DbConnection;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author David
+ */
+public class DAOProfessor {
+
+    String professorName;
+    String professorLastName;
+    String professorEmail;
+    String professorUser;
+    String professorPassword;
+    int professorId;
+
+    public String LogInProfessor(String usr, String passw) {
+        String resultado = null;
+        try {
+
+            String logIn = "SELECT professorName, professorLastName FROM Professor WHERE professorUser = '" + usr + "' AND professorPassword = '" + passw + "'";
+            System.out.println("La sentencia es : " + logIn);
+            DbConnection db = new DbConnection();
+            ResultSet rs = db.runSqlStatement(logIn);
+            if (rs.next()) {
+
+                professorName = rs.getString("professorName");
+                professorLastName = rs.getString("professorLastname");
+                resultado = professorName + " " + professorLastName;
+
+                return resultado;
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(DAOProfessor.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return resultado;
+    }
+
+    public String registryProfessor(String name, String user, String lastName, String email, String password) {
+        String resultado = "No se pudo ejecutar el registro";
+
+        String query = "INSERT INTO Professor(professorName, professorUser, professorLastName, professorEmail, professorPassword) VALUES (\"" + name + "\",\"" + user + "\",\"" + lastName + "\",\"" + email + "\",\"" + password + "\")";
+        System.out.println("La sentencia es : " + query);
+        DbConnection db = new DbConnection();
+        int rs = db.runSqlUpdate(query);
+
+        System.out.println("RESULT SET = " + rs);
+        if (rs != 0) {
+            System.out.println("Result Set = " + rs);
+            resultado = "Registro Completo";
+            return resultado;
+        }
+        return resultado;
+
+
+    }
+
+    public String searchProfessor(String name) {
+
+        String resultado = null;
+        String query = "SELECT * FROM Professor WHERE professorName = '" + name + "'";
+        
+        try {
+
+            DbConnection db = new DbConnection();
+            ResultSet rs = db.runSqlStatement(query);
+            if (rs.next()) {
+                
+                resultado = rs.getString("idProfessor");
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(DAOProfessor.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return resultado;
+    }
+}
---
+++ src/java/Dao/DAOStudent.java
@@ -0,0 +1,74 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Dao;
+
+import connection.DbConnection;
+import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author David
+ */
+public class DAOStudent {
+
+    String studentName;
+    String studentLastName;
+    String studentEmail;
+    String studentUser;
+    String studentPassword;
+    int studentId;
+
+    public String LogInStudent(String usr, String passw) {
+        String resultado = null;
+        try {
+            String logIn = "SELECT studentName, studentLastName FROM Student WHERE studentUser = '" + usr + "' AND studentPassword = '" + passw + "'";
+            DbConnection db = new DbConnection();
+            ResultSet rs = db.runSqlStatement(logIn);
+            if (rs.next()) {
+                studentName = rs.getString("studentName");
+                studentLastName = rs.getString("studentLastname");
+                resultado = studentName + " " + studentLastName;
+                return resultado;
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(DAOProfessor.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return resultado;
+    }
+
+    public String registryStudent(String name, String user, String lastName, String email, String password, String professor) {
+
+
+        String idProfessor = null;
+        String resultado = "No se pudo ejecutar el registro, Valide sus datos";
+
+        DAOProfessor DAOp = new DAOProfessor();
+        idProfessor = DAOp.searchProfessor(professor);
+
+        if (idProfessor != null) {
+
+            String query = "INSERT INTO Student(studentName, studentUser, studentLastName, studentEmail, studentPassword, idProfessor) VALUES (\"" + name + "\",\"" + user + "\",\"" + lastName + "\",\"" + email + "\",\"" + password + "\", " + idProfessor + " )";
+            System.out.println("La sentencia es : " + query);
+
+            DbConnection db = new DbConnection();
+            int rs = db.runSqlUpdate(query);
+            System.out.println("RESULT SET = " + rs);
+
+            if (rs != 0) {
+                System.out.println("Result Set = " + rs);
+                resultado = "Registro Completo";
+            }
+
+        }
+
+        return resultado;
+
+
+    }
+}
